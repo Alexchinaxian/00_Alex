8cbffce6ad06a0cafdd9da93a242534fde5816f9
xiaohexin <xiaohexin@cvte.com>
2024-08-15 10:34:11 +0800

    002

---
+++ Application/src/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <String.h>
+#include <Assert.h>
+int main(void)
+{
+    BSP_Init();
+    while(1)
+    {
+
+    }
+}
+
---
+++ BSP/inc/BSP_CAN.h
@@ -0,0 +1,10 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __BSP_CAN_H
+#define __BSP_CAN_H
+
+/* Includes ------------------------------------------------------------------*/
+//驱动层
+void BSP_CAN_Init(void);
+//业务层
+
+#endif /* __BSP_CAN_H */
